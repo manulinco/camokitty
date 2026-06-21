a204099c74c55b301300b1eb1e9212c3d529ebdf
MoreMode66 <MoreMode66@users.noreply.github.com>
2026-06-22 07:51:22 +0800

    feat: change cat poses to pixel person shapes

--- src/lib/levels.ts
+++ src/lib/levels.ts
@@ -7,40 +7,30 @@ export const BACKGROUNDS = [
 ];
 
 export const POSES = [
-  { 
-    id: 'pose-sit', 
-    name: 'Sitting',
-    path: 'M 80 70 Q 95 60 90 40 Q 85 20 75 35 Q 70 45 75 60 Z M 30 80 Q 50 85 70 80 L 75 50 Q 50 40 25 50 Z' 
+  {
+    id: 'pose-stand',
+    name: 'Standing Blocky',
+    path: 'M 40 10 L 60 10 L 60 30 L 40 30 Z M 35 30 L 65 30 L 65 65 L 35 65 Z M 20 30 L 35 30 L 35 60 L 20 60 Z M 65 30 L 80 30 L 80 60 L 65 60 Z M 35 65 L 48 65 L 48 95 L 35 95 Z M 52 65 L 65 65 L 65 95 L 52 95 Z'
   },
-  { 
-    id: 'pose-curl', 
-    name: 'Curled Up',
-    // A croissant/circle shape with ears
-    path: 'M 30 60 C 20 30 80 30 70 60 C 60 90 40 90 30 60 Z M 40 40 L 35 25 L 50 35 M 60 40 L 65 25 L 50 35' 
+  {
+    id: 'pose-tpose',
+    name: 'T-Pose',
+    path: 'M 40 10 L 60 10 L 60 30 L 40 30 Z M 40 30 L 60 30 L 60 70 L 40 70 Z M 10 30 L 40 30 L 40 45 L 10 45 Z M 60 30 L 90 30 L 90 45 L 60 45 Z M 40 70 L 48 70 L 48 95 L 40 95 Z M 52 70 L 60 70 L 60 95 L 52 95 Z'
   },
-  { 
-    id: 'pose-stretch', 
-    name: 'Stretched Out',
-    // Long pill shape
-    path: 'M 20 40 L 80 40 C 90 40 90 60 80 60 L 20 60 C 10 60 10 40 20 40 Z M 25 40 L 20 25 L 35 35 M 75 40 L 80 25 L 65 35' 
+  {
+    id: 'pose-sit',
+    name: 'Sitting Blocky',
+    path: 'M 40 20 L 60 20 L 60 40 L 40 40 Z M 40 40 L 60 40 L 60 75 L 40 75 Z M 60 60 L 90 60 L 90 75 L 60 75 Z M 25 40 L 40 40 L 40 70 L 25 70 Z'
   },
-  { 
-    id: 'pose-belly', 
-    name: 'Belly Up',
-    // Rounded bottom, 4 legs pointing up
-    path: 'M 20 60 Q 50 80 80 60 L 80 40 Q 50 20 20 40 Z M 30 45 L 30 20 M 45 50 L 45 20 M 55 50 L 55 20 M 70 45 L 70 20 M 20 50 L 10 30 L 30 40 M 80 50 L 90 30 L 70 40' 
+  {
+    id: 'pose-lay',
+    name: 'Laying Flat',
+    path: 'M 10 40 L 30 40 L 30 60 L 10 60 Z M 30 40 L 70 40 L 70 60 L 30 60 Z M 70 40 L 95 40 L 95 60 L 70 60 Z M 40 60 L 60 60 L 60 75 L 40 75 Z'
   },
-  { 
-    id: 'pose-sploot', 
-    name: 'Splooting',
-    // Flat belly, legs out sideways
-    path: 'M 20 40 Q 50 30 80 40 L 80 60 Q 50 70 20 60 Z M 25 45 L 5 45 M 75 45 L 95 45 M 35 60 L 25 80 M 65 60 L 75 80 M 50 35 L 40 20 L 55 30' 
-  },
-  { 
-    id: 'pose-hang', 
-    name: 'Hanging',
-    // One arm extended up, body hanging down
-    path: 'M 50 20 L 50 50 M 40 50 Q 40 90 50 90 Q 60 90 60 50 Z M 45 30 L 30 20 L 40 40 M 55 30 L 70 20 L 60 40 M 45 80 L 30 90 M 55 80 L 70 90' 
+  {
+    id: 'pose-wave',
+    name: 'Waving Blocky',
+    path: 'M 40 10 L 60 10 L 60 30 L 40 30 Z M 35 30 L 65 30 L 65 65 L 35 65 Z M 65 30 L 80 30 L 80 60 L 65 60 Z M 20 10 L 35 10 L 35 40 L 20 40 Z M 35 65 L 48 65 L 48 95 L 35 95 Z M 52 65 L 65 65 L 65 95 L 52 95 Z'
   }
 ];
 
